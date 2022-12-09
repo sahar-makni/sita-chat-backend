09e39a2667f2097f4e967a22bfb7e097451d231f
sahar makni <sahar.makni@squeezer-software.com>
2022-12-10 00:40:52 +0100

    add header access-token to CORS Filter

--- src/main/java/com/site/tech/filter/CORSFilter.java
+++ src/main/java/com/site/tech/filter/CORSFilter.java
@@ -12,7 +12,7 @@ public class CORSFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext creq, ContainerResponseContext cres) {
 
         cres.getHeaders().add("Access-Control-Allow-Origin", "*");
-        cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, access-token");
         cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
         cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD");
         cres.getHeaders().add("Access-Control-Max-Age", "1209600");
